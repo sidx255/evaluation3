aebf7767bf0bc32ec14e31238a8b6e82616ed946
sidx255 <siddharth190820@gmail.com>
2023-03-01 17:54:07 +0530

    fix: conditional rendering

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -19,7 +19,6 @@ const HomePage = () => {
 
   const [Events, setEvents] = React.useState<Array<[]>>([]);
   const [Themes, setThemes] = React.useState<Array<[]>>([]);
-  const [isClicked, setIsClicked] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     makeRequest(GET_ALL_EVENTS)
@@ -92,50 +91,43 @@ const HomePage = () => {
   };
 
   return (
-    !isClicked ?
-      <div className='body'>
-        <div className='util-bar'>
-          <div className='bar-left'>
-            <div className='filter-button'>
-              <p>FILTER</p>
-            </div>
-            <div className='button-registered-events' onClick={handleRegistered}>
-              <p>REGISTERED</p>
-            </div>
-            <div className='button-show-all-events' onClick={handleAll}>
-              <p>ALL</p>
-            </div>
+    <div className='body'>
+      <div className='util-bar'>
+        <div className='bar-left'>
+          <div className='filter-button'>
+            <p>FILTER</p>
           </div>
-          <div className='bar-right' onClick={handleBookmarked}>
-            <div className='search-bar' onClick={handleSearch}>
-              <input type='text' placeholder='Event Name'/>
-            </div>
-
-            <div className='button-show-bookmarked' onClick={handleBookmarked}>
-              <p>BOOKMARKED</p>
-            </div>
-            <div className='button-show-available-seats' onClick={handleAvailableSeats}>
-              <p>SEATS AVAILABLE</p>
-            </div>
+          <div className='button-registered-events' onClick={handleRegistered}>
+            <p>REGISTERED</p>
+          </div>
+          <div className='button-show-all-events' onClick={handleAll}>
+            <p>ALL</p>
+          </div>
+        </div>
+        <div className='bar-right' onClick={handleBookmarked}>
+          <div className='search-bar' onClick={handleSearch}>
+            <input type='text' placeholder='Event Name'/>
           </div>
 
+          <div className='button-show-bookmarked' onClick={handleBookmarked}>
+            <p>BOOKMARKED</p>
+          </div>
+          <div className='button-show-available-seats' onClick={handleAvailableSeats}>
+            <p>SEATS AVAILABLE</p>
+          </div>
         </div>
 
-        <div className='cards'>
-          {Events.map((event: any, idx: number) => (
-            // <Card setIsClicked={setIsClicked} key={idx} {...event} />
-            <Card setIsClicked={setIsClicked} key={idx} {...event} />
-          ))}
-        </div>
-        <br/>
       </div>
-      :
-      <div onClick={() => { setIsClicked(false);}}>
-        {
-          <p>lol</p>
-        }
 
+      <div className='cards'>
+        {Events.map((event: any, idx: number) => (
+          // <Card setIsClicked={setIsClicked} key={idx} {...event} />
+          <Card key={idx} {...event} />
+        ))}
       </div>
+      <br/>
+    </div>
+    
         
 
   );
